4d047284d07688bc0623a0fdc940e7e00c8938f0
TechNikFuture <108083158+TechNikFuture@users.noreply.github.com>
2024-01-29 08:44:18 +0100

    Update Originaltext Vokabeln.js

--- Originaltext Vokabeln.js
+++ Originaltext Vokabeln.js
@@ -1,71 +1,202 @@
 var Vokabeltest_Lektion = [
 
 
-    // Originaltext S. 28
 
-    {'first':'oppūngnāre', 
-    'second':'angreifen', 
-    'third':'oppūgnō, oppūgnāvī, oppūgnātum (regelmäßig)'},
 
-    {'first':'coniūrāre', 
-    'second':'verschworen, durch Eid verbunden', 
-    'third':'coniūrō, coniūrāvī, coniūrātum'},
 
-    {'first':'respondēre', 
-    'second':'hier= prophezeien', 
-    'third':'respondeō, respondī, respōnsum'},
 
-    {'first':'vīcēsimus', 
-    'second':'der Zwanzigste', 
-    'third':'-a, -um'},
 
-    {'first':'ōrātor', 
-    'second':'Redner', 
-    'third':'ōrātōris m.'},
+// Text 1 Lupus et agnus
+[
+{'first':'agnus',
+'second':'das Lamm',
+'third':'agni m.'},
 
-    {'first':'īnsānia', 
-    'second':'Wahnsinn', 
-    'third':'-ae f.'},
 
-    {'first':'simulāre', 
-    'second':'vortäuschen', 
-    'third':'simulō, simulāvī, simulātum (regelmäßig)'},
+{'first':'faux',
+'second':'der Rachen, der Schlund, die Kehle',
+'third':'faucis f.'},
 
-    {'first':'pileus', 
-    'second':'Kappe', 
-    'third':''},
+{'first':'incitare',
+'second':'antreiben',
+'third':'-o, -avi, -atum (regelmäßig)'},
 
-    {'first':'bōs', 
-    'second':'Rind', 
-    'third':'bovis m./f.'},
+{'first':'latro',
+'second':'der Räuber',
+'third':'latrois m.'},
 
-    {'first':'iūngere', 
-    'second':'verbinden', 
-    'third':'iūngō, iūnxī, iūnctum'},
+{'first':'lupus',
+'second':'der Wolf',
+'third':'lupi m.'},
 
-    {'first':'arātrum', 
-    'second':'Pflug', 
-    'third':'-ī n.'},
+{'first':'ait',
+'second':'behaupte ich, sage ich',
+'third':'3. Pers. Sg. ait, 3. Pers. Pl. aiunt'},  // "3. Pers. Sg." und "3. Pers. Pl." kursiv
 
-    {'first':'cūnae', 
-    'second':'Wiege', 
-    'third':'-ārum f.'},
+{'first':'ante',
+'second':'1. vor (Präp. + Akk.); 2. vorher (Adv.)',
+'third':''},
 
-    {'first':'subicere', 
-    'second':'davorwerfen', 
-    'third':'subiciō, subiēcī, subiectum'},
+{'first':'atque',
+'second':'und; und auch',
+'third':''},
 
-    {'first':'simulātiō', 
-    'second':'Verstellung', 
-    'third':'simulātiōnis f.'},
+{'first':'bibere',
+'second':'trinken',
+'third':'bibo, bibi, ---'},
 
-    {'first':'dēpōnere', 
-    'second':'ablegen', 
-    'third':'dēpōnō, dēposuī, dēpositum'},
+{'first':'causa',
+'second':'1. der Grund, die Ursache; 2. das Gerichtsverfahren, der Prozess',
+'third':'causae f.'},
 
-    {'first':'tunc', 
-    'second':'', 
-    'third':'da, dann'},
+{'first':'contra',
+'second':'dagegen, gegenüber',
+'third':'(Adv.)'},  // "(Adv.)" kursiv
+
+{'first':'corripere',
+'second':'ergreifen, gewaltsam an sich reißen',
+'third':'corripi, corripui, correptum'},
+
+{'first':'cur',
+'second':'warum?',
+'third':''},
+
+{'first':'dicere',
+'second':'sagen, sprechen; (be)nennen',
+'third':'dico, dixi, dictum'},
+
+{'first':'equidem',
+'second':'allerdings, freilich',
+'third':''},
+
+{'first':'facere',
+'second':',achen, tun, handeln',
+'third':'facio, feci, factum'},
+
+{'first':'fingere',
+'second':'gestalten, sich etwas ausdenken',
+'third':'fingo, finxi, fictum'},
+
+{'first':'hic',
+'second':'dieser',
+'third':'haec, hoc'},
+
+{'first':'idem',
+'second':'derselbe',
+'third':'eadem, idem'},
+
+{'first':'ille',
+'second':'jener',
+'third':'illa, illud'},
+
+{'first':'improbus',
+'second':'schlecht, unanständig',
+'third':'-a, -um (Adj.)'},
+
+{'first':'inferre',
+'second':'hineintragen, zufügen',
+'third':'infero, intuli, illatum'},
+
+{'first':'inquit',
+'second':'sagt er/sie, sagte er /sie',
+'third':''},
+
+{'first':'ita',
+'second':'1. so; 2. ja (in einer Antwort)',
+'third':''},
+
+{'first':'longus',
+'second':'1. lang; 2. weit',
+'third':'-a, -um (Adj.)'},
+
+{'first':'malus',
+'second':'schlecht, übel, schlimm',
+'third':'-a,-um (Adj.)'},
+
+{'first':'mensis',
+'second':'der Monat',
+'third':'mensis m.'},
+
+{'first':'nasci',
+'second':'geboren werden, enstehen',
+'third':'nascor, natus sum, ---'},
+
+{'first':'nex',
+'second':'der Mord, Tod',
+'third':'necis f.'},
+
+{'first':'opprimere',
+'second':'1. unterdrücken; 2. bekämpfen, überfallen; 3. bedorohen',
+'third':'opprimo, oppressi, oppressum'},
+
+{'first':'posse',
+'second':'können',
+'third':'possum, potui, ---'},
+
+{'first':'propter',
+'second':'wegen',
+'third':''},
+
+{'first':'queri',
+'second':'beklagen, sich beklagen',
+'third':'queror, questus sum'},
+
+{'first':'qui',
+'second':'der, welcher (Relativpronomen)',
+'third':'quae, quod'},
+
+{'first':'repellere',
+'second':'zurückstoßen, abweisen, vertreiben',
+'third':'repello, repulli, repulsum'},
+
+{'first':'respondere',
+'second':'1. antworten; 2. entsprechen',
+'third':'respondeo, respondi, responsum'},
+
+{'first':'scribere',
+'second':'schreiben, zeichnen',
+'third':'scribo, scripsi, scriptum'},
+
+{'first':'stare',
+'second':'stehen',
+'third':'sto, steti, statum'},
+
+{'first':'superior',
+'second':'der frühere, der weiter oben gelegene',
+'third':'superior, superius'},
+
+{'first':'timere',
+'second':'fürchten',
+'third':'timeo, timui, ---'},
+
+{'first':'tunc',
+'second':'1. dann, darauf; 2. damals',
+'third':'(Adv.)'},
+
+{'first':'venire',
+'second':'kommen',
+'third':'venio, veni, ventum'},
+
+{'first':'vis',
+'second':'1. die Kraft; 2. die Gewalt; 3. Pl.= auch Streitkräfte',
+'third':'Akk. vim, Abl. vi, Pl.= vires; virium f.'}, // "Akk.", "Abl." und "Pl.=" kursiv
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
