b0aeea768d62789d03c0548ef12e7c7effbe0731
Tech Nik <tech.nik.effects@gmail.com>
2024-04-05 19:25:30 +0200

    Stable v. 2.1.7

--- Originaltext Vokabeln.js
+++ Originaltext Vokabeln.js
@@ -1,178 +1,212 @@
 var Vokabeltest_Lektion = [
     
-    {'first':'agnus',
-    'second':'das Lamm',
-    'third':'i m.'},
-    
-    
-    {'first':'faux',
-    'second':'der Rachen, der Schlund, die Kehle',
-    'third':'faucis f.'},
-    
-    {'first':'incitare',
-    'second':'antreiben',
-    'third':'-o, -avi, -atum (regelmäßig)'},
-    
-    {'first':'latro',
-    'second':'der Räuber',
-    'third':'latrois m.'},
-    
-    {'first':'lupus',
-    'second':'der Wolf',
-    'third':'i m.'},
-    
-    {'first':'ait',
-    'second':'behaupte ich, sage ich',
-    'third':'3. Pers. Sg. ait, 3. Pers. Pl. aiunt'},  // "3. Pers. Sg." und "3. Pers. Pl." kursiv
-    
-    {'first':'ante',
-    'second':'1. vor (Präp. + Akk.) <br> 2. vorher (Adv.)',
+    {'first':'Skateboardanlage',
+    'second':'skatepark',
     'third':''},
     
-    {'first':'atque',
-    'second':'und; und auch',
+    {'first':'Lounge; Aufenthaltsraum',
+    'second':'lounge',
     'third':''},
     
-    {'first':'bibere',
-    'second':'trinken',
-    'third':'bibo, bibi, ---'},
-    
-    {'first':'causa',
-    'second':'1. der Grund, die Ursache; <br> 2. das Gerichtsverfahren, der Prozess',
-    'third':'ae f.'},
-    
-    {'first':'contra',
-    'second':'dagegen, gegenüber',
-    'third':'(Adv.)'},  // "(Adv.)" kursiv
-    
-    {'first':'corripere',
-    'second':'ergreifen, gewaltsam an sich reißen',
-    'third':'corripio, corripui, correptum'},
-    
-    {'first':'cur',
-    'second':'warum?',
+    {'first':'Zentrale; Hauptsitz',
+    'second':'headquarters (pl)',
     'third':''},
     
-    {'first':'dicere',
-    'second':'sagen, sprechen; (be)nennen',
-    'third':'dico, dixi, dictum'},
-    
-    {'first':'equidem',
-    'second':'allerdings, freilich',
+    {'first':'Skateboardfahrer/-in',
+    'second':'skateboarder',
     'third':''},
     
-    {'first':'facere',
-    'second':'machen, tun, handeln',
-    'third':'facio, feci, factum'},
-    
-    {'first':'fingere',
-    'second':'gestalten, sich etwas ausdenken',
-    'third':'fingo, finxi, fictum'},
-    
-    {'first':'hic',
-    'second':'dieser',
-    'third':'haec, hoc'},
-    
-    {'first':'idem',
-    'second':'derselbe',
-    'third':'eadem, idem'},
-    
-    {'first':'ille',
-    'second':'jener',
-    'third':'illa, illud'},
-    
-    {'first':'improbus',
-    'second':'schlecht, unanständig',
-    'third':'-a, -um (Adj.)'},
-    
-    {'first':'inferre',
-    'second':'hineintragen, zufügen',
-    'third':'infero, intuli, illatum'},
-    
-    {'first':'inquit',
-    'second':'sagt er / sie, <br> sagte er / sie',
+    {'first':'Tendenz',
+    'second':'tendency',
     'third':''},
     
-    {'first':'ita',
-    'second':'1. so; 2. ja (in einer Antwort)',
+    {'first':'mexikanisch; <br> aus Mexiko; Mexikaner/-in',
+    'second':'Mexican',
     'third':''},
     
-    {'first':'longus',
-    'second':'1. lang; 2. weit',
-    'third':'-a, -um (Adj.)'},
-    
-    {'first':'malus',
-    'second':'schlecht, übel, schlimm',
-    'third':'-a,-um (Adj.)'},
-    
-    {'first':'mensis',
-    'second':'der Monat',
-    'third':'mensis m.'},
-    
-    {'first':'nasci',
-    'second':'geboren werden, enstehen',
-    'third':'nascor, natus sum, ---'},
-    
-    {'first':'nex',
-    'second':'der Mord, Tod',
-    'third':'necis f.'},
-    
-    {'first':'opprimere',
-    'second':'1. unterdrücken; 2. bekämpfen, überfallen; <br> 3. bedrohen',
-    'third':'opprimo, oppressi, oppressum'},
-    
-    {'first':'posse',
-    'second':'können',
-    'third':'possum, potui, ---'},
-    
-    {'first':'propter',
-    'second':'wegen',
+    {'first':'Topf',
+    'second':'pot',
     'third':''},
     
-    {'first':'queri',
-    'second':'beklagen, sich beklagen',
-    'third':'queror, questus sum'},
-    
-    {'first':'qui',
-    'second':'der, welcher (Relativpronomen)',
-    'third':'quae, quod'},
-    
-    {'first':'repellere',
-    'second':'zurückstoßen, abweisen, vertreiben',
-    'third':'repello, repulli, repulsum'},
-    
-    {'first':'respondere',
-    'second':'1. antworten; 2. entsprechen',
-    'third':'respondeo, respondi, responsum'},
-    
-    {'first':'scribere',
-    'second':'schreiben, zeichnen',
-    'third':'scribo, scripsi, scriptum'},
-    
-    {'first':'stare',
-    'second':'stehen',
-    'third':'sto, steti, statum'},
-    
-    {'first':'superior',
-    'second':'der frühere, der weiter oben gelegene',
-    'third':'superior, superius'},
-    
-    {'first':'timere',
-    'second':'fürchten',
-    'third':'timeo, timui, ---'},
-    
-    {'first':'tunc',
-    'second':'1. dann, darauf; 2. damals',
-    'third':'(Adv.)'},
-    
-    {'first':'venire',
-    'second':'kommen',
-    'third':'venio, veni, ventum'},
-    
-    {'first':'vis',
-    'second':'1. die Kraft; 2. die Gewalt; <br> 3. Pl.= Streitkräfte',
-    'third':'Akk. vim, Abl. vi, <br> Pl.= vires; virium f.'}, // "Akk.", "Abl." und "Pl.=" kursiv
 
+
+    
+    {'first':'Vernunft, Verstand',
+    'second':'reason',
+    'third':''},
+    
+    {'first':'Gouverneur/-in',
+    'second':'gevernor',
+    'third':''},
+    
+    {'first':'beobachten, beachten, befolgen',
+    'second':'to observe',
+    'third':''},
+    
+    {'first':'aufstrebend',
+    'second':'aspiring',
+    'third':''},
+    
+    {'first':'Glück',
+    'second':'luck',
+    'third':''},
+    
+    {'first':'schlendern; bummeln; spazieren',
+    'second':'to stroll',
+    'third':''},
+    
+    {'first':'So; auf diese Weise',
+    'second':'that way',
+    'third':''},
+    
+    {'first':'Glanz und Glamour',
+    'second':'glitz and glamor (no pl)',
+    'third':''},
+    
+    {'first':'sich über Wasser halten',
+    'second':'to keep one`s head above water',
+    'third':''},
+    
+    {'first':'sein Talent unter Beweis stellen',
+    'second':'to showcase one`s talent',
+    'third':''},
+    
+    {'first':'Glückstag',
+    'second':'lucky day',
+    'third':''},
+    
+    {'first':'Vorspielen; Vorsingen',
+    'second':'cattle call',
+    'third':''},
+    
+    {'first':'Möchtegern; Möchtegern-',
+    'second':'wannabe (infml)',
+    'third':''},
+    
+    {'first':'zusammenpacken',
+    'second':'to pack up',
+    'third':''},
+    
+    {'first':'etw. als Erfahrung verbuchen',
+    'second':'to chalk sth up to experience',
+    'third':''},
+    
+    {'first':'Premiere; Uraufführung',
+    'second':'premiere',
+    'third':''},
+    
+    {'first':'für Aufsehen sorgen',
+    'second':'to cause a stir',
+    'third':''},
+    
+    {'first':'Campingplatz',
+    'second':'RV park',
+    'third':''},
+    
+    {'first':'drehen (Film)',
+    'second':'to shoot',
+    'third':''},
+    
+    {'first':'explodieren',
+    'second':'to explode',
+    'third':''},
+    
+    {'first':'Wohnwagen',
+    'second':'trailer',
+    'third':''},
+    
+    {'first':'Statist/-in; <br> Komparse / Komparsin',
+    'second':'extra',
+    'third':''},
+    
+    {'first':'worum es geht',
+    'second':'what it´s all about',
+    'third':''},
+    
+    {'first':'Bemühung; Mühe',
+    'second':'effort',
+    'third':''},
+    
+
+
+
+
+    {'first':'verfolgen',
+    'second':'to pursue',
+    'third':''},
+    
+    {'first':'hartnäckig; ausdauernd; beharrlich',
+    'second':'persistent',
+    'third':''},
+    
+    {'first':'entspannt; locker; gelassen',
+    'second':'relaxed',
+    'third':''},
+    
+    {'first':'geeignet; passend',
+    'second':'suitable',
+    'third':''},
+    
+    {'first':'Agent/-in',
+    'second':'agent',
+    'third':''},
+    
+    {'first':'nehmen (Zug, Bus)',
+    'second':'to catch, caught, caught',
+    'third':''},
+    
+    {'first':'backstage; hinter der Bühne',
+    'second':'backstage',
+    'third':''},
+    
+    {'first':'denkwürdig; unvergesslich',
+    'second':'memorable',
+    'third':''},
+    
+    {'first':'Gründer/-in',
+    'second':'founder',
+    'third':''},
+    
+    {'first':'umfassen; beinhalten; einschließen',
+    'second':'to comprise',
+    'third':''},
+    
+    {'first':'Produzent/-in',
+    'second':'producer',
+    'third':''},
+    
+    {'first':'Beitrag; Beteiligung',
+    'second':'contribution',
+    'third':''},
+    
+    {'first':'solange',
+    'second':'as long as',
+    'third':''},
+    
+    {'first':'vorgeschlagene(r) Kandidat(in)',
+    'second':'nominee',
+    'third':''},
+    
+    {'first':'nominieren; ernennen',
+    'second':'to nominate',
+    'third':''},
+    
+    {'first':'Nominierung; Ernennung',
+    'second':'nomination',
+    'third':''},
+    
+    {'first':'anerkennen; gutheißen; genehmigen',
+    'second':'to approve',
+    'third':''},
+    
+    {'first':'zu Ehren',
+    'second':'in honor of',
+    'third':''},
+    
+    {'first':'Zeichentrick-',
+    'second':'animated',
+    'third':''},
+    
 
 
 
@@ -393,7 +427,7 @@ var s_8_tantalus = [
 'second':'melden', 
 'third':'renuntiare, -o, -avi, -atum'},
 
-{'first':'bracchium ', 
+{'first':'bracchium', 
 'second':'Arm', 
 'third':'bracchium, i n.'},
 
@@ -413,7 +447,7 @@ var s_8_tantalus = [
 'second':'ebenso', 
 'third':''},
 
-{'first':'super ', 
+{'first':'super', 
 'second':'über', 
 'third':'(+Akk)'},
 
@@ -705,545 +739,3 @@ var Lupus_et_agnus = [
 
 ]
 
-
-var Englisch_Vokabeltest_ersten_70 = [
-
-
-{'first':'einem Angriff ausweichen', 
-'second':'dodge an attack', 
-'third':''},
-
-{'first':'eine Kette von Explosionen auslösen', 
-'second':'set off a chain of explosions', 
-'third':''},
-
-{'first':'auf jem zielen', 
-'second':'aim for', 
-'third':''},
-
-{'first':'Falle', 
-'second':'trap', 
-'third':''},
-
-{'first':'Rivalität zwischen', 
-'second':'rivalry between', 
-'third':''},
-
-{'first':'ein leichtes Ziel', 
-'second':'an easy target', 
-'third':''},
-
-{'first':'Hass', 
-'second':'hatred', 
-'third':''},
-
-{'first':'Wert', 
-'second':'value', 
-'third':''},
-
-{'first':'übrig bleiben', 
-'second':'remain', 
-'third':''},
-
-{'first':'Regeländerung', 
-'second':'rule change', 
-'third':''},
-
-{'first':'sich feindlich gegenüberstehen', 
-'second':'confront one another', 
-'third':''},
-
-{'first':'von Angesicht zu Angesicht', 
-'second':'face-to-face', 
-'third':''},
-
-{'first':'manipulieren', 
-'second':'manipulate', 
-'third':''},
-
-{'first':'Manipulation', 
-'second':'manipulation', 
-'third':''},
-
-{'first':'Kontrolle haben über', 
-'second':'have control over', 
-'third':''},
-
-{'first':'Schwäche zeigen', 
-'second':'show weakness', 
-'third':''},
-
-{'first':'Mitleid', 
-'second':'pity', 
-'third':''},
-
-{'first':'Hilfe, Unterstützung', 
-'second':'aid', 
-'third':''},
-
-{'first':'Bewunderung', 
-'second':'admiration', 
-'third':''},
-
-{'first':'Weigerung', 
-'second':'refusal', 
-'third':''},
-
-{'first':'leicht zu entdecken', 
-'second':'easy to spot', 
-'third':''},
-
-{'first':'Geschenk', 
-'second':'gift', 
-'third':''},
-
-{'first':'Verräter', 
-'second':'traitor', 
-'third':''},
-
-{'first':'jem von etwas ablenken', 
-'second':'distract sb from', 
-'third':''},
-
-{'first':'albern aussehen', 
-'second':'look foolish', 
-'third':''},
-
-{'first':'Wespennest', 
-'second':'wasp nest', 
-'third':''},
-
-{'first':'Halluzinationen hervorgerufen <br> durch das Gift', 
-'second':'hallucinations brought <br> on by the venom', 
-'third':''},
-
-{'first':'Stich', 
-'second':'sting', 
-'third':''},
-
-{'first':'Salbe', 
-'second':'ointment', 
-'third':''},
-
-{'first':'Blase', 
-'second':'blister', 
-'third':''},
-
-{'first':'die Salbe verteilen', 
-'second':'spread the balm', 
-'third':''},
-
-{'first':'von Baum zu Baum springen', 
-'second':'leap from tree to tree', 
-'third':''},
-
-{'first':'Chaos, Durcheinander', 
-'second':'mayhem', 
-'third':''},
-
-{'first':'hysterisch zucken', 
-'second':'twitch hysterically', 
-'third':''},
-
-{'first':'eine Kanone feuert', 
-'second':'a cannon fires', 
-'third':''},
-
-{'first':'ohnmächtig werden', 
-'second':'black out', 
-'third':''},
-
-{'first':'Behandlung', 
-'second':'treatment for sth', 
-'third':''},
-
-{'first':'die Ernte essen', 
-'second':'eat the crops', 
-'third':''},
-
-{'first':'jdn auspeitschen', 
-'second':'whip sb', 
-'third':''},
-
-{'first':'sich spezialisieren auf', 
-'second':'specialise in sth', 
-'third':''},
-
-{'first':'Landwirtschaft', 
-'second':'agriculture', 
-'third':''},
-
-{'first':'unterdrückt', 
-'second':'oppressed', 
-'third':''},
-
-{'first':'Unterdrückung', 
-'second':'oppression', 
-'third':''},
-
-{'first':'Bestrafung', 
-'second':'punishment', 
-'third':''},
-
-{'first':'harmlos', 
-'second':'harmless', 
-'third':''},
-
-{'first':'Aufstand', 
-'second':'uprising', 
-'third':''},
-
-{'first':'ein Gespräch unterdrücken', 
-'second':'block out a conversation', 
-'third':''},
-
-{'first':'die Notwendigkeit der Flucht', 
-'second':'the need for flight', 
-'third':''},
-
-{'first':'Albtraum', 
-'second':'nightmare', 
-'third':''},
-
-{'first':'an den Stichen sterben', 
-'second':'die from the stings', 
-'third':''},
-
-{'first':'nur noch zähe Gegner übrig', 
-'second':'only tough opponents left', 
-'third':''},
-
-{'first':'etwas gegenüberstehen', 
-'second':'face sth', 
-'third':''},
-
-{'first':'Wunde', 
-'second':'wound', 
-'third':''},
-
-{'first':'Schnitt', 
-'second':'cut', 
-'third':''},
-
-{'first':'wund', 
-'second':'sore', 
-'third':''},
-
-{'first':'Blätter kauen', 
-'second':'chew leaves', 
-'third':''},
-
-{'first':'Erleichterung', 
-'second':'relief', 
-'third':''},
-
-{'first':'ein befristetes Abkommen', 
-'second':'a temporary deal', 
-'third':''},
-
-{'first':'eine öffentliche Auspeitschung', 
-'second':'a public whipping', 
-'third':''},
-
-{'first':'Wurzeln und Nüsse sammeln', 
-'second':'gather roots and nuts', 
-'third':''},
-
-{'first':'Ausweichen, Umgehung', 
-'second':'evasion', 
-'third':''},
-
-// Ersten 61 bis hier hin
-
-
-{'first':'ausdenken, entwerfen', 
-'second':'devise', 
-'third':''},
-
-{'first':'jdn ausspionieren', 
-'second':'spy on sb', 
-'third':''},
-
-{'first':'jem schwächen', 
-'second':'weaken sb', 
-'third':''},
-
-{'first':'in jem Schuld stehen', 
-'second':'be in sb\'s debt', 
-'third':''},
-
-{'first':'Schau', 
-'second':'an act', 
-'third':''},
-
-{'first':'Theater spielen / eine Schau abziehen', 
-'second':'to put on act', 
-'third':''},
-
-{'first':'die Vorräte reichen für', 
-'second':'supplies last for x days', 
-'third':''},
-
-{'first':'etwas ergattern', 
-'second':'get hold of sth', 
-'third':''},
-
-{'first':'Ich kann nicht anders, <br> als an ihn zu denken', 
-'second':'I can\'t help thinking of him', 
-'third':''},
-
-{'first':'aufmerksam', 
-'second':'observant', 
-'third':''},
-
-]
-
-
-
-
-
-var Englisch_Vokabeltest_rest = [
-
-
-
-    {'first':'schwer verletzt', 
-    'second':'wounded badly', 
-    'third':''},
-
-    {'first':'ein hängender Sack voller Äpfel', 
-    'second':'a hanging sack of apples', 
-    'third':''},
-
-    {'first':'Landmine', 
-    'second':'land mine', 
-    'third':''},
-
-    {'first':'ausgraben', 
-    'second':'dig up', 
-    'third':''},
-
-    {'first':'Lageraufbau', 
-    'second':'camp setup', 
-    'third':''},
-
-    {'first':'reaktivieren', 
-    'second':'reactivate', 
-    'third':''},
-
-    {'first':'etwas unbeaufsichtigt lassen', 
-    'second':'leave sth unprotected', 
-    'third':''},
-
-    {'first':'versteckte Sprengladung', 
-    'second':'booby trap', 
-    'third':''},
-
-    {'first':'vermint', 
-    'second':'mined', 
-    'third':''},
-
-    {'first':'mit einem Sprengsatz versehen', 
-    'second':'booby-trapped', 
-    'third':''},
-
-    {'first':'Vorratslager', 
-    'second':'a stash of supplies', 
-    'third':''},
-
-    {'first':'ein Sonderbeitrag über etwas', 
-    'second':'a feature on sth', 
-    'third':''},
-
-    {'first':'die Spannung erhöhen', 
-    'second':'raise the tension', 
-    'third':''},
-
-    {'first':'gegen jem antreten', 
-    'second':'take sb on', 
-    'third':''},
-
-    {'first':'wirklich', 
-    'second':'for real', 
-    'third':''},
-
-    {'first':'Raute', 
-    'second':'rue', 
-    'third':''},
-
-    {'first':'jem abschrecken', 
-    'second':'scare off sb', 
-    'third':''},
-
-    {'first':'Begleiter', 
-    'second':'companion to sb', 
-    'third':''},
-
-    {'first':'für etwas vorteilhaft sein', 
-    'second':'beneficial to sth', 
-    'third':''},
-
-    {'first':'Schöpfer', 
-    'second':'creator', 
-    'third':''},
-
-    {'first':'eine Leiche herausholen', 
-    'second':'retrieve a body', 
-    'third':''},
-
-    {'first':'betäuben', 
-    'second':'deafen', 
-    'third':''},
-
-    {'first':'Melodie', 
-    'second':'tune', 
-    'third':''},
-
-    {'first':'Speer', 
-    'second':'spear', 
-    'third':''},
-
-    {'first':'eine wichtige <br> Veränderung kennzeichnen', 
-    'second':'mark an important shift', 
-    'third':''},
-
-    {'first':'Rache', 
-    'second':'revenge', 
-    'third':''},
-
-    {'first':'der Wunsch nach etwas', 
-    'second':'desire for sth', 
-    'third':''},
-
-    {'first':'jem beschämen', 
-    'second':'shame sb', 
-    'third':''},
-
-    {'first':'unter Druck geraten', 
-    'second':'come under pressure', 
-    'third':''},
-
-    {'first':'jem zur Rechenschaft ziehen', 
-    'second':'make sb accountable', 
-    'third':''},
-
-    {'first':'liebevoll', 
-    'second':'affectionately', 
-    'third':''},
-
-    {'first':'zum Gewinner erklärt werden', 
-    'second':'be declared the winner', 
-    'third':''},
-
-    {'first':'leben, am Leben sein', 
-    'second':'be alive', 
-    'third':''},
-
-    {'first':'von etwas profitieren', 
-    'second':'benefit from sth', 
-    'third':''},
-
-    {'first':'unsterblich verliebt sein', 
-    'second':'to be madly in love', 
-    'third':''},
-
-    {'first':'Romanze', 
-    'second':'romance', 
-    'third':''},
-
-    {'first':'Fälschung', 
-    'second':'fake', 
-    'third':''},
-
-    {'first':'verletzt sein', 
-    'second':'be wounded', 
-    'third':''},
-
-    {'first':'vermeiden', 
-    'second':'avoid', 
-    'third':''},
-
-
-
-
-    {'first':'jem am Leben erhalten', 
-    'second':'keep sb alive', 
-    'third':''},
-
-    {'first':'sich erweisen als', 
-    'second':'turn out to be', 
-    'third':''},
-
-    {'first':'stechen', 
-    'second':'sting', 
-    'third':''},
-
-    {'first':'erstechen', 
-    'second':'stab', 
-    'third':''},
-
-    {'first':'Unkraut', 
-    'second':'weeds', 
-    'third':''},
-
-    {'first':'schlammig', 
-    'second':'muddy', 
-    'third':''},
-
-    {'first':'Schlamm', 
-    'second':'mud', 
-    'third':''},
-
-    {'first':'schlammverkrustet', 
-    'second':'caked with mud', 
-    'third':''},
-
-    {'first':'entzündet', 
-    'second':'inflamed', 
-    'third':''},
-
-    {'first':'entwässern', 
-    'second':'drain', 
-    'third':''},
-
-    {'first':'Sieger', 
-    'second':'victor', 
-    'third':''},
-
-    {'first':'gestellter Kuss', 
-    'second':'a staged kiss', 
-    'third':''},
-
-    {'first':'die Haare bürsten', 
-    'second':'brush the hair', 
-    'third':''},
-
-    {'first':'etw. streicheln', 
-    'second':'stroke sth', 
-    'third':''},
-
-    {'first':'Eiter', 
-    'second':'pus', 
-    'third':''},
-
-    {'first':'Blutvergiftung', 
-    'second':'blood poisoning', 
-    'third':''},
-
-    {'first':'Schwellung', 
-    'second':'swelling', 
-    'third':''},
-
-    {'first':'Ziege', 
-    'second':'goat', 
-    'third':''},
-
-    {'first':'Beschützer', 
-    'second':'protector', 
-    'third':''},
-
-    {'first':'Brühe', 
-    'second':'broth', 
-    'third':''},
-
-
-]
